76cf4c6b250fdae302df5eed8172182aebb0de0d
mason555 <31895126+mason555@users.noreply.github.com>
2021-01-26 09:27:42 -0600

    Add files via upload

---
+++ wpaperchange.ps1
@@ -0,0 +1,11 @@
+﻿$Wallpaper = "wpaper.jpg"
+
+$strPath = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies"
+$strPath1 = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+
+
+New-Item -Path $strPath -Name System -Force
+New-Item -Path $strPath1 -Name Wallpaper
+New-Item -Path $strPath1 -Name WallpaperStyle
+Set-ItemProperty -Path $strPath -Name Wallpaper -value "\\dc1\share1\powershell\background\wpaper.jpg"
+Set-ItemProperty -Path $strPath -Name WallpaperStyle -value "2"
