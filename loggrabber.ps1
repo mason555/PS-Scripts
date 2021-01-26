f52d8532b73604b2896eb3228629421ce8f6ec0f
mason555 <31895126+mason555@users.noreply.github.com>
2021-01-26 09:12:35 -0600

    Add files via upload

---
+++ loggrabber.ps1
@@ -0,0 +1,9 @@
+﻿$filename = $env:COMPUTERNAME
+
+$files = @('CAS.log', 'LocationServices.log', 'AppDiscovery.log', 'CCMExec.log')
+
+foreach ($file in $files) {
+    Get-ChildItem -Path "C:\Windows\CCM\Logs" -Include $file -Recurse | 
+        Copy-Item -Destination "\\REMOTECOMPUTERNAME\c$\RemoteLogStore"
+
+}
